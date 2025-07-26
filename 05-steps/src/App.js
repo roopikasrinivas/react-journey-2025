a1fdd4a9e97db87df5f423b7750d292324601874
roopikasrinivas <roopika.srinivas@gmail.com>
2025-07-26 19:42:36 +0530

    WIP

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   //state and hooks
   const [step, setStep] = useState(1);
 
@@ -31,7 +40,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -66,6 +75,6 @@ export default function App() {
           </div>
         </>
       )}
-    </>
+    </div>
   );
 }
---
+++ 06-counter/src/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import "./Counter.css";
+
+export default function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+
+  const [count, setCount] = useState(0);
+
+  const [date, setDate] = useState(new Date().toDateString());
+
+  const [dayCounter, setDayCounter] = useState(0);
+
+  useEffect(() => {
+    const updatedDate = new Date();
+    updatedDate.setDate(updatedDate.getDate() + step * count);
+    setDate(updatedDate.toDateString());
+    setDayCounter(step * count);
+  }, [step, count]);
+
+  const reset = () => {
+    setStep(1);
+    setCount(0);
+  };
+
+  return (
+    <div className="counter-container">
+      <div className="counter-row">
+        <button onClick={() => setStep((s) => (s > 1 ? s - 1 : s))}>-</button>
+        <h2 className="animated">Step: {step}</h2>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+
+      <div className="counter-row">
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <h2 className="animated">Count: {count}</h2>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+      </div>
+
+      <DateString dayCounter={dayCounter} date={date} />
+
+      <button className="reset-button" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+function DateString({ dayCounter, date }) {
+  let dateString;
+  if (dayCounter === 0) {
+    dateString = `Today is ${date}`;
+  } else if (dayCounter > 0) {
+    dateString = `${dayCounter} days from today is ${date}`;
+  } else {
+    dateString = `${dayCounter * -1} days ago was today is ${date}`;
+  }
+
+  return <h2>{dateString}</h2>;
+}
