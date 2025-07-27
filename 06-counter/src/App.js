3e84a1cffb0316cc7213f72d5510f9ae0da34315
roopikasrinivas <roopika.srinivas@gmail.com>
2025-07-27 13:58:25 +0530

    WIP

--- 06-counter/src/App.js
+++ 06-counter/src/App.js
@@ -27,17 +27,46 @@ function Counter() {
   };
 
   return (
+    // <div className="counter-container">
+    //   <div className="counter-row">
+    //     <button onClick={() => setStep((s) => (s > 1 ? s - 1 : s))}>-</button>
+    //     <h2 className="animated">Step: {step}</h2>
+    //     <button onClick={() => setStep((s) => s + 1)}>+</button>
+    //   </div>
+
+    //   <div className="counter-row">
+    //     <button onClick={() => setCount((c) => c - 1)}>-</button>
+    //     <h2 className="animated">Count: {count}</h2>
+    //     <button onClick={() => setCount((c) => c + 1)}>+</button>
+    //   </div>
+
+    //   <DateString dayCounter={dayCounter} date={date} />
+
+    //   <button className="reset-button" onClick={reset}>
+    //     Reset
+    //   </button>
+    // </div>
+
     <div className="counter-container">
       <div className="counter-row">
-        <button onClick={() => setStep((s) => (s > 1 ? s - 1 : s))}>-</button>
-        <h2 className="animated">Step: {step}</h2>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {step}
       </div>
 
       <div className="counter-row">
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
-        <h2 className="animated">Count: {count}</h2>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => Number(c) - 1)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => Number(c) + 1)}>+</button>
       </div>
 
       <DateString dayCounter={dayCounter} date={date} />
