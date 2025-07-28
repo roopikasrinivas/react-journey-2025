2de35111c65b9445ff6f1d11d89c2da864000882
roopikasrinivas <roopika.srinivas@gmail.com>
2025-07-28 22:53:12 +0530

    WIP

---
+++ practice/travel-list/src/App.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+  const [items, setItem] = useState([]);
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ FAR AWAY 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <p>What do you need for your 😍 trip? </p>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text"></input>
+      <button>ADD</button>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have X items on your list, and you already packed X (X%)
+    </footer>
+  );
+}
