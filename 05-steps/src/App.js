b47d9e0b2d9a15525cdb7cc2925db72abc91f635
roopikasrinivas <roopika.srinivas@gmail.com>
2025-08-03 23:24:00 +0530

    WIP

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
     </div>
   );
 }
@@ -59,22 +58,32 @@ function Steps() {
             </p>
 
             <div className="buttons">
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              <Button
+                bgColor="#7950f2"
+                textColor="#fff"
                 onClick={handlePrevious}
               >
-                Previous
-              </button>
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                onClick={handleNext}
-              >
-                Next
-              </button>
+                <span>👈</span>Previous
+              </Button>
+
+              <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+                Next<span>👉</span>
+              </Button>
             </div>
           </div>
         </>
       )}
     </div>
+  );
+}
+
+function Button({ bgColor, textColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
