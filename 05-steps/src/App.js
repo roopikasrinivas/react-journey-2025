e299a35706a698081f7396de964ea64136c448d3
roopikasrinivas <roopika.srinivas@gmail.com>
2025-08-03 23:31:25 +0530

    WIP

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -53,9 +53,7 @@ function Steps() {
               <div className={step >= 3 ? "active" : ""}>3</div>
             </div>
 
-            <p className="message">
-              Step {step}: {messages[step - 1]}
-            </p>
+            <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
             <div className="buttons">
               <Button
@@ -73,6 +71,15 @@ function Steps() {
           </div>
         </>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
