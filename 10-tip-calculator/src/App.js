ff79cd426627af914c9d09cc311e4d81ff703903
Roopika Srinivas <roopika.srinivas@gmail.com>
2025-08-05 15:52:50 +0530

    WIP

--- 10-tip-calculator/src/App.js
+++ 10-tip-calculator/src/App.js
@@ -2,114 +2,79 @@ import { useState } from "react";
 import "./index.css";
 
 export default function App() {
-  const [billAmount, setBillAmount] = useState(0);
+  return <TipCalculator />;
+}
 
-  const [userRating, setUserRating] = useState("dissatisfied");
+function TipCalculator() {
+  const [bill, setBill] = useState(0);
 
-  const [userFriendRating, setUserFriendRating] = useState("dissatisfied");
+  const [percent1, setPercent1] = useState(0);
 
-  const [tipAmount, setTipAmount] = useState(0);
+  const [percent2, setPercent2] = useState(0);
 
-  function handleBillInput(amount) {
-    setBillAmount(amount);
-    calculateTip(amount, userRating, userFriendRating);
-  }
-
-  function handleUserRatingChange(rating) {
-    setUserRating(rating);
-    calculateTip(billAmount, rating, userFriendRating);
-  }
-
-  function handleFriendRatingChange(rating) {
-    setUserFriendRating(rating);
-    calculateTip(billAmount, userRating, rating);
-  }
-
-  function calculateTip(
-    newBillAmount = billAmount,
-    newUserRating = userRating,
-    newFriendRating = userFriendRating
-  ) {
-    const tipPercent =
-      (getTipPercent(newUserRating) + getTipPercent(newFriendRating)) / 2;
-
-    const tip = newBillAmount * (tipPercent / 100);
-
-    setTipAmount(newBillAmount > 0 ? tip : 0);
-  }
-
-  function getTipPercent(rating) {
-    if (rating === "dissatisfied") {
-      return 0;
-    } else if (rating === "okay") {
-      return 5;
-    } else if (rating === "good") {
-      return 10;
-    } else return 20;
-  }
+  const tip = bill * ((percent1 + percent2) / 2 / 100);
 
   function handleReset() {
-    setBillAmount(0);
-    setUserRating("dissatisfied");
-    setUserFriendRating("dissatisfied");
-    setTipAmount(0);
+    setBill(0);
+    setPercent1(0);
+    setPercent2(0);
   }
+
   return (
     <div className="App">
-      <BillInput billAmount={billAmount} onBillInput={handleBillInput} />
-      <div className="rating">
-        <h3>How did you like the service?</h3>
-        <SelectPercent
-          key={1}
-          rating={userRating}
-          onRatingChange={handleUserRatingChange}
-        />
-      </div>
-      <div className="rating">
-        <h3>How did your friend like the service?</h3>
-        <SelectPercent
-          key={2}
-          rating={userFriendRating}
-          onRatingChange={handleFriendRatingChange}
-        />
-      </div>
-      <BillOutput billAmount={billAmount} tipAmount={tipAmount} />
-      <Reset onReset={handleReset} />
+      <BillInput bill={bill} setBill={setBill} />
+      <SelectPercent percent={percent1} setPercent={setPercent1}>
+        How did you like the service?
+      </SelectPercent>
+
+      <SelectPercent percent={percent2} setPercent={setPercent2}>
+        How did your friend like the service?
+      </SelectPercent>
+
+      {bill > 0 && (
+        <>
+          <BillOutput bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
 
-function BillInput({ billAmount, onBillInput }) {
+function BillInput({ bill, setBill }) {
   return (
     <div className="bill">
-      <h3>How much was the bill?</h3>
+      <label>How much was the bill? </label>
       <input
         type="text"
-        id="billAmount"
-        value={billAmount}
-        onChange={(e) => onBillInput(Number(e.target.value))}
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
       ></input>
     </div>
   );
 }
 
-function SelectPercent({ rating, onRatingChange }) {
+function SelectPercent({ percent, setPercent, children }) {
   return (
     <div>
-      <select value={rating} onChange={(e) => onRatingChange(e.target.value)}>
-        <option value="dissatisfied">Dissatisfied (0%)</option>
-        <option value="okay">It was okay (5%)</option>
-        <option value="good">It was good (10%)</option>
-        <option value="amazing">Absolutely amazing! (20%)</option>
+      <label>{children} </label>
+      <select
+        value={percent}
+        onChange={(e) => setPercent(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="10">It was okay (5%)</option>
+        <option value="15">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
 }
 
-function BillOutput({ billAmount, tipAmount }) {
+function BillOutput({ bill, tip }) {
   return (
     <h2>
-      You pay ${billAmount} (${billAmount} + ${tipAmount} tip)
+      You pay ${bill + tip} (${bill} + ${tip} tip)
     </h2>
   );
 }
